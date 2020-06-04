a0ebdcedc752fdad441495cf72471469f1e787f8
Prayson W. Daniel <prayson.wilfred@itelligence.dk>
2020-06-04 15:55:57 +0200

    new pypi version with data remove from hisia.

--- hisia/__init__.py
+++ hisia/__init__.py
@@ -6,19 +6,19 @@ from pathlib import Path
 store_model = Path.cwd() / 'hisia/models/base_model.pkl'
 store_stops = Path.cwd() / 'hisia/data/stops.pkl'
 
-# if not store_model.exists():
-#     from shutil import move
-#     import hisia
+if not store_model.exists():
+    from shutil import move
+    import hisia
      
-#     base = Path(hisia.__file__).parent
-#     store_model.parent.mkdir(parents=True, exist_ok=True)
-#     store_stops.parent.mkdir(parents=True, exist_ok=True)
+    base = Path(hisia.__file__).parent
+    store_model.parent.mkdir(parents=True, exist_ok=True)
+    store_stops.parent.mkdir(parents=True, exist_ok=True)
 
-#     locate_model = base / 'models/base_model.pkl'
-#     locate_stops =base / 'data/stops.pkl'
+    locate_model = base / 'models/base_model.pkl'
+    locate_stops =base / 'data/stops.pkl'
 
-#     move(locate_model, store_model)  
-#     move(locate_stops, store_stops)
+    move(locate_model, store_model)  
+    move(locate_stops, store_stops)
   
 
 from .hisia import Hisia
